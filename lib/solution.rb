f8414e3edad1d79bd269fbeffe4a7f60160ccf68
Kim Nguyen <nnhk23@gmail.com>
2020-10-20 09:52:33 -0700

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def clock_angle(time)
-    # code your solution here
+    hour = time.split(":")[0].to_f
+    min = time.split(":")[1].to_f
+    total_min = (hour * 60) + min
+    degree = (total_min/2) - (min*6)    
+    if degree == 360
+        degree = 0
+    elsif degree < 0
+        degree += 360 
+    else
+        degree
+    end
 end
